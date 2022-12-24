4633edfba87999ed24c4cbcc348621df6415b55b
Sid4737 <sidhonwar99@gmail.com>
2022-12-24 17:26:57 +0530

    Addition function
    Addition function

--- 03_function.js
+++ 03_function.js
@@ -8,7 +8,17 @@ function sachin(money){
 
  var veg = sachin(500);
  console.log(veg);
+ console.log("===========");
 
+   function functionName(city, street, age, is_married){
+   console.log(city, street, age, is_married);
+   return "city + street + age + is_married "
+}
+
+  var value = functionName("Pune","Khawgalli","22","No");
+  console.log(value);
+
+console.log("==================");
 
  function tech(author,sign){
        console.log("this book is very good");
@@ -17,3 +27,28 @@ function sachin(money){
  }
     var structure = tech("reader","index");
     console.log(structure);
+
+
+     function swap_values(arg1,arg2){
+         console.log("======Before swap=======");
+         console.log(arg1,arg2);
+         console.log("=======After swap=======");
+         var temp = arg1;
+         arg1 = arg2;
+         arg2=temp;
+         console.log(arg1,arg2);
+ }    
+
+      swap_values("Virat","Anushka");
+      swap_values("Mango","Apple");
+      swap_values(100,300);
+
+     console.log("========Addition function======");
+
+     function addition(arg1, arg2, arg3){
+          console.log(arg1 + arg2 + arg3);
+
+     }
+         addition(200, 300, 600);
+         addition("Sachin"," Ramesh"," Tendulkar")
+         addition("Hello"," good"," Morning")
