0c17e2adc11c83da51b48c63c37ac6e5c7983973
Sid4737 <sidhonwar99@gmail.com>
2023-01-24 21:35:07 +0530

    NaN operator & its functions
    NaN operator & its functions

---
+++ 04_NaN..js
@@ -0,0 +1,17 @@
+
+var resultDivide = 0/0;
+console.log(`Result is : ${resultDivide}`);
+console.log(`Type of ${resultDivide} is ${typeof resultDivide}`);
+
+
+var resultAdd = resultDivide + 10;
+console.log(`resultAdd is : ${resultAdd}`); 
+
+var resultMul = resultDivide * 10;
+console.log(`resultMul is : ${resultMul}`); 
+
+var num = new Number("100");
+console.log(`num is : ${num}`);
+
+var num = new Number("Prathmesh");
+console.log(`num is : ${num}`);
